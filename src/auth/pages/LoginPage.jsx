8a30bb6a299dea83d67624c51c25f3724170ac87
Silvana Zalazar <90283693+SilvanaZ@users.noreply.github.com>
2022-09-23 21:02:22 -0300

    adding changes on LoginPage

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,22 +1,11 @@
-<<<<<<< HEAD
-import { useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-
-import { useGoogleLogin } from '@react-oauth/google';
-import Swal from 'sweetalert2';
-
-import { useForm, useAuthStore } from '../../hooks';
-=======
 import React, { useEffect } from "react"
 import { Link as RouterLink } from "react-router-dom";
 import { Link } from '@mui/material';
 import axios from "axios";
 import Swal from "sweetalert2";
 import { useGoogleLogin } from "@react-oauth/google";
-import {LoginGithub} from "react-login-github";
+import { LoginGithub } from "react-login-github";
 import { useForm, useAuthStore } from "../../hooks";
->>>>>>> 8843db6beb5af11aa8f4e2b210dbadbf7f395c3d
 import './LoginPage.css';
 
 
@@ -26,13 +15,9 @@ const loginFormFields = {
 }
 
 export const LoginPage = () => {
-<<<<<<< HEAD
-
-=======
     console.log()
->>>>>>> 8843db6beb5af11aa8f4e2b210dbadbf7f395c3d
     const { errorMessage, startLogin } = useAuthStore();
-    const { loginEmail, loginPassword, onInputChange: onLoginInputChange } = useForm( loginFormFields );
+    const { loginEmail, loginPassword, onInputChange: onLoginInputChange } = useForm(loginFormFields);
 
     const login = useGoogleLogin({
         onSuccess: async (response) => {
@@ -61,40 +46,13 @@ export const LoginPage = () => {
 
 
     const loginSubmit = (event) => {
-
         event.preventDefault();
-
         startLogin({
             email: loginEmail,
             password: loginPassword,
         });
+
     };
-
-    const login = useGoogleLogin({
-        onSuccess: async (response) => {
-            try {
-                const res = await axios.get(
-                    "https://www.googleapis.com/oauth2/v3/userinfo",
-                    {
-                        headers: {
-                            Authorization: `Bearer ${response.access_token}`,
-                        },
-                    }
-                );
-                console.log(res.data);
-                startLogin({
-                    name: res.data.name,
-                    email: res.data.email,
-                    password: res.data.sub,
-                    avatar: res.data.picture,
-                    google: true,
-                });
-            } catch (error) {
-                console.log(error);
-            }
-        },
-    });
-
     useEffect(() => {
         if (errorMessage !== undefined) {
             Swal.fire('Error en la autenticación', errorMessage, 'error');
@@ -103,105 +61,67 @@ export const LoginPage = () => {
 
 
     return (
-<<<<<<< HEAD
+
+
         <div className="container">
             <div className="container-login">
                 <div className='group-form'>
                     <p>Ingreso</p>
-                    <form >
+                    <form onSubmit={loginSubmit}>
                         <div className="group-form">
                             <div className="icon-form">
                                 <i className="fa-solid fa-envelope"></i>
-=======
-
-
-            <div className="container">
-                <div className="container-login">
-                    <div className='group-form'>
-                        <p>Ingreso</p>
-                        <form onSubmit={loginSubmit}>
-                            <div className="group-form">
-                                <div className="icon-form">
-                                    <i className="fa-solid fa-envelope"></i>
-                                </div>
-                                <input
-                                    type="email"
-                                    placeholder="Email"
-                                    name="loginEmail"
-                                    value={loginEmail}
-                                    onChange={onLoginInputChange}
-                                />
->>>>>>> 8843db6beb5af11aa8f4e2b210dbadbf7f395c3d
                             </div>
-                            <div className="group-form">
-                                <div className="icon-form">
-                                    <i className="fa-solid fa-lock"></i>
-                                </div>
-                                <input
-                                    type="password"
-                                    placeholder="Contraseña"
-                                    name="loginPassword"
-
-                                    value={loginPassword}
-                                    onChange={onLoginInputChange}
-                                />
-                            </div>
-                            <div className="d-grid gap-2">
-                                <button className="button-login"
-                                    type="submit">
-                                    Ingresar
-                                </button>
-                            </div>
-                        </form>
-                        <p className="text-2">o continuar con estos perfiles sociales</p>
-                    </div>
-                    <div className="container-icon">
-                        <div className="icon-style">
-                            <i className="fa-brands fa-github"></i>
+                            <input
+                                type="email"
+                                placeholder="Email"
+                                name="loginEmail"
+                                value={loginEmail}
+                                onChange={onLoginInputChange}
+                            />
                         </div>
-<<<<<<< HEAD
+                        <div className="group-form">
+                            <div className="icon-form">
+                                <i className="fa-solid fa-lock"></i>
+                            </div>
+                            <input
+                                type="password"
+                                placeholder="Contraseña"
+                                name="loginPassword"
+
+                                value={loginPassword}
+                                onChange={onLoginInputChange}
+                            />
+                        </div>
                         <div className="d-grid gap-2">
-                            <button 
-                                className="button-login"
-                                /* type="submit" */
-                                onClick={ loginSubmit }
-                                    >
-                                    <Link to={'/profile'}>Ingresar</Link>
-                                </button>
-                       
+                            <button className="button-login"
+                                type="submit">
+                                Ingresar
+                            </button>
                         </div>
                     </form>
                     <p className="text-2">o continuar con estos perfiles sociales</p>
                 </div>
-                <div className="continer-icon">
+                <div className="container-icon">
                     <div className="icon-style">
                         <i className="fa-brands fa-github"></i>
                     </div>
-                    <button className="icon-style" onClick={login}>
+                    <div className="icon-style" onClick={login}>
                         <i className="fa-brands fa-google"></i>
-                    </button>
-                </div>
-                <p className="text-2">¿No tienes una cuenta? 
-                    <Link 
-                        to='/auth/register'>
-=======
-                        <div className="icon-style" onClick={login}>
-                            <i className="fa-brands fa-google"></i>
-                        </div>
                     </div>
-                    <p className="text-2">¿No tienes una cuenta?
-                        <Link
-                            component={RouterLink}
-                            color='inherit'
-                            to='/auth/register'>
->>>>>>> 8843db6beb5af11aa8f4e2b210dbadbf7f395c3d
-                            <span>Registrate</span>
-                        </Link>
-                    </p>
-
                 </div>
+                <p className="text-2">¿No tienes una cuenta?
+                    <Link
+                        component={RouterLink}
+                        color='inherit'
+                        to='/auth/register'>
+                        <span>Registrate</span>
+                    </Link>
+                </p>
+
             </div>
-    
+        </div>
+
 
     );
 };
