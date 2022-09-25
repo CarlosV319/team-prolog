c2b8402910cf13cf81599911298b8db4386883d8
Silvana Zalazar <90283693+SilvanaZ@users.noreply.github.com>
2022-09-25 01:07:25 -0300

    details of styles in ProfilePage.css

--- src/profile/pages/ProfilePage.jsx
+++ src/profile/pages/ProfilePage.jsx
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux';
 import { useProfileStore } from '../../hooks/useProfileStore';
 import { Link } from 'react-router-dom';
 import '../css/ProfilePage.css'
-
+import Avatar from '@material-ui/core/Avatar';
 
 export const ProfilePage = () => {
 
@@ -16,61 +16,70 @@ export const ProfilePage = () => {
     }, []);
 
     return (
-        <div className="container-profile">
-            <div>
-                <button>
-                    <i className="fa-solid fa-right-from-bracket"></i>Salir
+        <>
+            <div className="titulo-profile">
+                <h1>Información personal</h1>
+                <p className="font-color">Información básica, como tu nombre y foto</p>
+            </div>
+            <div >
+                <button className="btn-logout">
+                    <i className="icon-mr fa-solid fa-right-from-bracket"></i>Salir
                 </button>
             </div>
-            <h1>Información personal</h1>
-            <p className="font-color">Información básica, como tu nombre y foto</p>
-            <div className="continer-profile-1">
-                <div>
-                    <div className="df-col">
-                        <div>
-                            <p className="font-l">Perfil</p>
-                            <p className="font-xs font-color">
-                                La información no puede ser visible para otras personas
-                            </p>
+
+            <div className="container-profile">
+
+
+                <div className="continer-profile-1">
+                    <div>
+                        <div className="df-col">
+                            <div>
+                                <p className="font-l">Perfil</p>
+                                <p className="sub-text">
+                                    La información no puede ser visible para otras personas
+                                </p>
+                            </div>
+                            <Link to={'/profile/' + profileUser?.uid}>
+                                <button className="btn-edit">Editar</button>
+                            </Link>
                         </div>
-                        <Link to={'/profile/' + profileUser?.uid}>
-                            <button className="btn-edit">Editar</button>
-                        </Link>
+                        <hr />
+                    </div>
+                    <div className="df-col">
+                        <p className="text-mr font-color">FOTO</p>
+                        <Avatar alt="SZ" src="/static/images/avatar/3.jpg" />
+                        <p className="text-mr font-color">{/* {profileUser?.avatar} */}</p>
                     </div>
                     <hr />
-                </div>
-                <div className="df-col">
-                    <p className="text-mr font-color">{profileUser?.avatar}</p>
-                    <i className="fa-solid fa-user-secret size"></i>
-                </div>
-                <hr />
-                <div className="df-col">
-                    <p className="text-mr font-color">NOMBRE</p>
-                    <p>{profileUser?.name}</p>
-                </div>
-                <hr />
-                <div className="df-col">
-                    <p className="text-mr font-color">BIOGRAFÍA</p>
-                    <p className="text-w">
-                        {profileUser?.bio}
-                    </p>
-                </div>
-                <hr />
-                <div className="df-col">
-                    <p className="text-mr font-color">TELÉFONO</p>
-                    <p>{profileUser?.phoneNumber}</p>
-                </div>{" "}
-                <hr />
-                <div className="df-col">
-                    <p className="text-mr font-color">EMAIL</p>
-                    <p>{profileUser?.email}</p>
-                </div>{" "}
-                <hr />
-                <div className="df-col">
-                    <p className="text-mr font-color">CONTRASEÑA</p>
-                    <p>{profileUser?.password}</p>
+                    <div className="df-col">
+                        <p className="text-mr font-color">NOMBRE</p>
+                        <p>{profileUser?.name}</p>
+                    </div>
+                    <hr />
+                    <div className="df-col">
+                        <p className="text-mr font-color">BIOGRAFÍA</p>
+                        <p className="text-w">
+                            {profileUser?.bio}
+                        </p>
+                    </div>
+                    <hr />
+                    <div className="df-col">
+                        <p className="text-mr font-color">TELÉFONO</p>
+                        <p>{profileUser?.phoneNumber}</p>
+                    </div>{" "}
+                    <hr />
+                    <div className="df-col">
+                        <p className="text-mr font-color">EMAIL</p>
+                        <p>{profileUser?.email}</p>
+                    </div>{" "}
+                    <hr />
+                    <div className="df-col">
+                        <p className="text-mr font-color">CONTRASEÑA</p>
+
+                        <p>{profileUser?.password}</p>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
