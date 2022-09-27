be9644219dca399b9aeaf9db63e344419b6ae585
mauriciofollonier <76795293+mauriciofollonier@users.noreply.github.com>
2022-09-27 04:13:56 -0300

    HashRouter deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Provider } from "react-redux";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 import { AppRouter } from "./router";
 import { store } from "./store";
@@ -13,11 +13,11 @@ export const App = () => {
   return (
     <>
       <Provider store={store}>
-        <BrowserRouter>
+        <HashRouter>
           <GoogleOAuthProvider clientId="460270658005-htl3tult9jvji943gkbtd4btams77pd4.apps.googleusercontent.com">
             <AppRouter />
           </GoogleOAuthProvider>
-        </BrowserRouter>
+        </HashRouter>
       </Provider>
     </>
   );
